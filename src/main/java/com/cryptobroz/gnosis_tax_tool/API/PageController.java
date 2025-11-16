8b8ffee0fbca70a9c73f20deadba254afc4c547d
tohveli <you@example.com>
2025-11-16 17:18:14 +0200

    wow thymeleaft

--- src/main/java/com/cryptobroz/gnosis_tax_tool/API/PageController.java
+++ src/main/java/com/cryptobroz/gnosis_tax_tool/API/PageController.java
@@ -1,12 +1,30 @@
 package com.cryptobroz.gnosis_tax_tool.API;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cryptobroz.gnosis_tax_tool.services.CashbackService.CashbackService;
+import com.cryptobroz.gnosis_tax_tool.services.CashbackService.dto.Cashback;
 
 @Controller
 public class PageController {
+
+  CashbackService cashbackService;
+
+  public PageController(CashbackService cashbackService) {
+    this.cashbackService = cashbackService;
+  }
+
   @GetMapping("/")
-  public String index() {
+  public String index(Model model) {
+
+    List<Cashback> cashbacks = cashbackService.getCashbackPrices();
+
+    model.addAttribute("cashbacks", cashbacks);
+
     return "index";
   }
 }
